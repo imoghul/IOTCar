1a50dbc8d8222556e9b65d1356d741875ec17274
imoghul <moghul02@gmail.com>
2022-04-20 11:52:27 -0400

    more tuning

--- Project 10/wheels.c
+++ Project 10/wheels.c
@@ -26,7 +26,7 @@ extern int rightVals[VALUES_TO_HOLD];
 
 
 PIDController rightFollowController = {
-    .kP = 14,// /8
+    .kP = 5,// /8
     .kD = 3,// /8
     //.kI = 0,
     .error = 0,
@@ -34,7 +34,7 @@ PIDController rightFollowController = {
     //.lastIntegral = 0
 };
 PIDController leftFollowController = {
-    .kP = 14,// /8
+    .kP = 5,// /8
     .kD = 3,// /8
     //.kI = 0,
     .error = 0,
